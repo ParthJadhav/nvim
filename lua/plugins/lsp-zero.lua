88dec38c322382e5ade19129366d5dfaec6f1661
Adib Hanna <adibhanna@gmail.com>
2023-10-29 22:50:09 -0500

    wip

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -286,43 +286,43 @@ return {
             },
           }),
 
-          lspconfig.gopls.setup({
-            settings = {
-              gopls = {
-                gofumpt = true,
-                codelenses = {
-                  gc_details = false,
-                  generate = true,
-                  regenerate_cgo = true,
-                  run_govulncheck = true,
-                  test = true,
-                  tidy = true,
-                  upgrade_dependency = true,
-                  vendor = true,
-                },
-                hints = {
-                  assignVariableTypes = true,
-                  compositeLiteralFields = true,
-                  compositeLiteralTypes = true,
-                  constantValues = true,
-                  functionTypeParameters = true,
-                  parameterNames = true,
-                  rangeVariableTypes = true,
-                },
-                analyses = {
-                  fieldalignment = true,
-                  nilness = true,
-                  unusedparams = true,
-                  unusedwrite = true,
-                  useany = true,
-                },
-                usePlaceholders = true,
-                completeUnimported = true,
-                staticcheck = true,
-                directoryFilters = {'-.git', '-.vscode', '-.idea', '-.vscode-test', '-node_modules'},
-              }
-            }
-          }),
+          -- lspconfig.gopls.setup({
+          --   settings = {
+          --     gopls = {
+          --       gofumpt = true,
+          --       codelenses = {
+          --         gc_details = false,
+          --         generate = true,
+          --         regenerate_cgo = true,
+          --         run_govulncheck = true,
+          --         test = true,
+          --         tidy = true,
+          --         upgrade_dependency = true,
+          --         vendor = true,
+          --       },
+          --       hints = {
+          --         assignVariableTypes = true,
+          --         compositeLiteralFields = true,
+          --         compositeLiteralTypes = true,
+          --         constantValues = true,
+          --         functionTypeParameters = true,
+          --         parameterNames = true,
+          --         rangeVariableTypes = true,
+          --       },
+          --       analyses = {
+          --         fieldalignment = true,
+          --         nilness = true,
+          --         unusedparams = true,
+          --         unusedwrite = true,
+          --         useany = true,
+          --       },
+          --       usePlaceholders = true,
+          --       completeUnimported = true,
+          --       staticcheck = true,
+          --       directoryFilters = {'-.git', '-.vscode', '-.idea', '-.vscode-test', '-node_modules'},
+          --     }
+          --   }
+          -- }),
           lspconfig.terraformls.setup({
             cmd = {'terraform-ls', 'serve'},
             filetypes = {'terraform', 'tf', 'terraform-vars'},
@@ -341,7 +341,7 @@ return {
           ['lua_ls'] = {'lua'},
           ['rust_analyzer'] = {'rust'},
           ['tsserver'] = {'javascript', 'typescript'},
-          ['gopls'] = {'go'},
+          -- ['gopls'] = {'go'},
         }
       })
 
